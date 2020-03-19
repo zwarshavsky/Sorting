1e633bc99e26e463ab830764e0a67a00bfaedde4
Zhenya Warshavsky <zwarshavsky@gmail.com>
2020-03-18 20:03:07 -0400

    updated not tested

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,17 +1,41 @@
+
+arr = [38,27,43,3,9,82,65,67]
+
+
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
-def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
-    merged_arr = [0] * elements
-    # TO-DO
-    
-    return merged_arr
+
+def merge(arrA, arrB):
+    # elements = len( arrA ) + len( arrB )
+    if len(arrA) == 1:
+        if arrA[0] > arrB[0]:
+            arrA[0],arrB[0] = arrB[0],arrA[0]
+            l = [arrA[0],arrB[0]]
+        else:
+            l = [arrA[0],arrB[0]]
+    else:
+        l = arrA + arrB
+        for i in range(0, len(l) - 1):
+            cur_index = i
+            smallest_index = cur_index
+            for j in range(cur_index, len(l)):
+                if l[smallest_index] > l[j]:
+                    l[smallest_index],l[j] = l[j],l[smallest_index]
+
+    return l
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
-def merge_sort( arr ):
-    # TO-DO
-
-    return arr
+def merge_sort(arr):
+    if len(arr) == 1:
+        print("base case reached:",arr)
+        return arr
+    else:
+        print("recursive case started:",arr)
+        middle = len(arr) // 2
+        left = merge_sort(arr[:middle])
+        right = merge_sort(arr[middle:])
+        print("recursive case reached:",left,right)
+        return (merge(left,right))
 
 
 # STRETCH: implement an in-place merge sort algorithm
@@ -31,3 +55,6 @@ def merge_sort_in_place(arr, l, r):
 def timsort( arr ):
 
     return arr
+
+if __name__ == "__main__":
+    print(merge_sort(arr))
